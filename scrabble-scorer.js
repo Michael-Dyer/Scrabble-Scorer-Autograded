0a8cc1ccde85aae5a2902c5b03194fcdc7cdcf9f
Michael Dyer <Dyerm636@gmail.com>
2024-07-05 21:54:44 -0500

    part 1 and 2 are complete

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -12,46 +12,117 @@ const oldPointStructure = {
   10: ['Q', 'Z']
 };
 
-function oldScrabbleScorer(word) {
+let oldScrabbleScorer = function(word) {
 	word = word.toUpperCase();
 	let letterPoints = "";
+   let points = 0;
  
 	for (let i = 0; i < word.length; i++) {
  
 	  for (const pointValue in oldPointStructure) {
  
 		 if (oldPointStructure[pointValue].includes(word[i])) {
-			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+			//letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+         points = points + parseInt(pointValue);
 		 }
  
 	  }
 	}
-	return letterPoints;
+   //old return statement
+	//return letterPoints;
+   return points;
  }
+
+ let simpleScorer = function(word){
+   let points = 0;
+	word = word.toUpperCase();
+   
+   for (let i = 0; i < word.length; i++) {
+      points++;
+     
+    }
+
+    return points;
+
+ }
+
+let vowelBonusScorer = function(word){
+   let points = 0;
+	word = word.toUpperCase();
+
+   for (let i = 0; i < word.length; i++) {
+      if (word[i] == 'A' || word[i] == 'E' || word[i] == 'I' || word[i] == 'O' || word[i] == 'U' || word[i] == 'Y'){
+         points = points + 3;
+      } 
+      else{
+         points++;
+      }
+     
+   }
+
+   return points;
+
+ }
+
 
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   let word = input.question("Let's play some scrabble! Enter a word:");
+   return word;
 };
 
 let newPointStructure;
 
-let simpleScorer;
+//let simpleScorer;
 
-let vowelBonusScorer;
+//let vowelBonusScorer;
 
 let scrabbleScorer;
 
-const scoringAlgorithms = [];
+const scoringAlgorithms = [
+   //0
+   {
+      name: "Simple Score",
+      description:   "Each letter is worth 1 point.",
+      scorerFunction:   simpleScorer
+   },
 
-function scorerPrompt() {}
+   //1
+   {
+      name: "Bonus Vowels",
+      description:   "Vowels are 3 pts, consonants are 1 pt.",
+      scorerFunction:   vowelBonusScorer
+   },
+
+   //2
+   {
+      name: "Scrabble",
+      description:   "The traditional scoring algorithm.",
+      scorerFunction:   oldScrabbleScorer
+   },
+
+   
+
+
+];
+
+function scorerPrompt(word) {
+
+   //note to self
+   //don't allow other options and make more descriptive
+   let option = input.question("Please Select a scoring option between 0-2! ");
+   option = parseInt(option);
+   console.log(scoringAlgorithms[option].scorerFunction(word));
+}
 
 function transform() {};
 
 function runProgram() {
-   initialPrompt();
+   let word = initialPrompt();
+
+   scorerPrompt(word);
    
 }
 
